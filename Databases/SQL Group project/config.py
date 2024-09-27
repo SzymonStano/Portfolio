4089ef35ce56e0b7f774a44785f42a0416199b28
SzymonStano <stanoszymon@gmail.com>
2024-09-27 18:38:22 +0200

    SQL project: minor fixes

--- Databases/SQL Group project/config.py
+++ Databases/SQL Group project/config.py
@@ -1,11 +1,11 @@
 import os
 from urllib.parse import quote_plus
 
-user = 'team08'
-password = 'te@m24ob'
+user = 'user'
+password = 'password'
 encoded_password = quote_plus(password)
-host = 'giniewicz.it'
-port = '3306'
-database = 'team08'
+host = 'host'
+port = 'port'
+database = 'database'
 
 DATABASE_URL = os.getenv('DATABASE_URL', f'mysql+pymysql://{user}:{encoded_password}@{host}:{port}/{database}')
